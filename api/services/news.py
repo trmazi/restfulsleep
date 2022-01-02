2e7cbca05d36d524caec4f65de7a73810c7eb9be
Trenton Zimmer <66042448+trmazi@users.noreply.github.com>
2022-01-01 19:58:27 -0500

    add news

---
+++ api/services/news.py
@@ -0,0 +1,17 @@
+from flask_restful import Resource, reqparse
+from datetime import datetime
+
+from api.data.mysql import MySQLBase
+
+class getAllNews(Resource):
+    def get(self):
+        data = MySQLBase.pull('news ORDER BY timestamp DESC')
+        dicts = []
+        for news in data:
+            unixtime = news[1]
+            humantime = datetime.utcfromtimestamp(unixtime).strftime('%Y-%m-%d')
+            v = {'id':news[0],'timestamp':humantime, 'title':news[2], 'body':news[3]}
+            dicts.append(v)
+        return {
+            'news':dicts
+        }, 200
--- api/utils/main.py
+++ api/utils/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask.helpers import get_root_path
 from flask_restful import Api
 from typing import Any, Dict
 import argparse
@@ -6,13 +7,16 @@ import argparse
 #import the services
 from api.services.login import guidesLoginStatus
 from api.services.user import userFromPIN
+from api.services.news import getAllNews
 
 app = Flask(__name__)
 api = Api(app)
 config: Dict[str, Any] = {}
 
+#add services
 api.add_resource(guidesLoginStatus, '/guidelogin')
 api.add_resource(userFromPIN, '/userfrompin')
+api.add_resource(getAllNews, '/getallnews')
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="PhaseII's powerful API, RestfulSleep. Built with Flask and restful.")
