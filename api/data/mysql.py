82d7b068be4f54015501d0e608ffccd4663ad335
Trenton Zimmer <66042448+trmazi@users.noreply.github.com>
2022-02-28 17:09:43 -0800

    Update mysql.py

--- api/data/mysql.py
+++ api/data/mysql.py
@@ -1,17 +1,29 @@
 import mysql.connector
 
 class MySQLBase():
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="bemani",
+        password="bemani",
+        database="bemani"
+    )
+
     def pull(type:str):
-        connection = mysql.connector.connect(
-            host="localhost",
-            user="bemani",
-            password="bemani",
-            database="bemani"
-        )
-        cursor = connection.cursor()
+        cursor = MySQLBase.connection.cursor()
         cursor.execute(f'SELECT * FROM {type}')
 
         return cursor.fetchall()
 
-    def push(type:str):
+    def getUser(userid: int):
+        cursor = MySQLBase.connection.cursor()
+        cursor.execute(f'SELECT username, email, admin, data FROM user WHERE id = {userid}')
+        return cursor.fetchone()
+
+    def putUserDiscordData(userid: int, discorddata: dict):
+        data = {
+            discorddata
+        }
+
+        cursor = MySQLBase.connection.cursor()
+        cursor.execute(f'UPDATE user SET data = {data} WHERE id = {userid}')
         return None
