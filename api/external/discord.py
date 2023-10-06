838878c7cb746741f670bdba05ab155fd9d78177
Trenton Zimmer <66042448+trmazi@users.noreply.github.com>
2023-10-06 19:54:22 -0400

    Redo discord

--- api/external/discord.py
+++ api/external/discord.py
@@ -1,15 +1,14 @@
-import discord
-import requests
+import time
+from discord_webhook import DiscordWebhook
 
 class GoodSaniacWebhook:
     def __init__(self) -> None:
-        self.webhook: discord.Webhook = discord.Webhook.partial(
-            1159993812966453289,
-            "cWqU5uxoNGHTPoOYgsAX11bl62lfu0pR277sZ1NUjfMlaQSpqw7wncpOkTKueBj6no-S"
-        )
+        self.webhookUrl = "https://discord.com/api/webhooks/1159993812966453289/cWqU5uxoNGHTPoOYgsAX11bl62lfu0pR277sZ1NUjfMlaQSpqw7wncpOkTKueBj6no-S"
 
     def sendVideoMessage(self, videoData: bytes):
-        self.webhook.send(
-            "Check out this new play video! It's too cool!",
-            file=discord.File(videoData)
+        Webhook = DiscordWebhook(
+            self.webhookUrl,
+            content = "Check out this new play video! It's too cool!" 
         )
+        Webhook.add_file(videoData, f'playVideo_{str(time.time())}.mp4')
+        Webhook.execute()
