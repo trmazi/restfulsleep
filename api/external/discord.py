4d77420ebfd9a23400f90f451ce0e96f64f484ce
Trenton Zimmer <66042448+trmazi@users.noreply.github.com>
2023-10-06 19:38:10 -0400

    Update discord.py

--- api/external/discord.py
+++ api/external/discord.py
@@ -5,8 +5,7 @@ class GoodSaniacWebhook:
     def __init__(self) -> None:
         self.webhook: discord.Webhook = discord.Webhook.partial(
             1159993812966453289,
-            "cWqU5uxoNGHTPoOYgsAX11bl62lfu0pR277sZ1NUjfMlaQSpqw7wncpOkTKueBj6no-S",
-            adapter=discord.RequestsWebhookAdapter()
+            "cWqU5uxoNGHTPoOYgsAX11bl62lfu0pR277sZ1NUjfMlaQSpqw7wncpOkTKueBj6no-S"
         )
 
     def sendVideoMessage(self, videoData: bytes):
