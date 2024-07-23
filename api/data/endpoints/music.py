ffb1dd078f5ecfa6307a03f664f3e1f74f0960f2
Trenton Zimmer <trentonzimmer@gmail.com>
2024-07-23 04:08:04 +0000

    Add cache to music data loading

--- api/data/endpoints/music.py
+++ api/data/endpoints/music.py
@@ -1,10 +1,26 @@
+import os
+import pickle
+import time
 from api.data.mysql import MySQLBase
-from api.data.types import Music, Attempt, Score
+from api.data.types import Music
 from api.data.json import JsonEncoded
 
 class MusicData:
     @staticmethod
     def getAllMusic(game: str = None, version: int = None, limit: int = None, chart: int = None, song_ids: list[int] = None) -> list[dict]:
+        cache_file = f'/var/restfulcache/music_{game}_{version}.pkl'
+        cache_lifetime = 1440 * 60  # 10 minutes in seconds
+
+        # Check if the cache file exists and if it's recent
+        if os.path.exists(cache_file):
+            file_age = time.time() - os.path.getmtime(cache_file)
+            if file_age < cache_lifetime:
+                # Load and return cached data
+                with open(cache_file, 'rb') as in_file:
+                    _, cached_data = pickle.load(in_file)
+                return cached_data
+
+        # Fetch new data from the database
         with MySQLBase.SessionLocal() as session:
             # Get the list of songs for a game or version
             musicQuery = (
@@ -33,5 +49,9 @@ class MusicData:
                     'genre': song.genre,
                     'data': JsonEncoded.deserialize(song.data)
                 })
+
+        # Cache the result data for faster reads
+        with open(cache_file, 'wb') as out_file:
+            pickle.dump((time.time(), musicData), out_file)
 
         return musicData
