b2697107582532b5aebaa4ca521ea0dff5699733
Trenton Zimmer <trentonzimmer@gmail.com>
2024-09-14 01:52:28 +0000

    Fix again

--- api/services/discord.py
+++ api/services/discord.py
@@ -48,7 +48,7 @@ class OnboardingVPN(Resource):
             if not discordId:
                 return APIConstants.bad_end('No Discord ID!')
 
-            file_content = ''.join(arcadeConfig[0]).encode('utf-8')
+            file_content = ''.join(list(arcadeConfig[0])).encode('utf-8')
             file_name = f"gradius-{arcadeConfig[1]}-phaseii-config.ovpn"
             files = {
                 'vpnFile': (file_name, BytesIO(file_content), 'text/plain')
