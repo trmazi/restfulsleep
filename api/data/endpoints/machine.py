2b3abcd797d77ab7836b1c4cf90f0f1aafaa0234
Trenton Zimmer <trentonzimmer@gmail.com>
2024-09-17 19:48:56 +0000

    Fix port setting

--- api/data/endpoints/machine.py
+++ api/data/endpoints/machine.py
@@ -51,7 +51,7 @@ class MachineData:
                 if newMachine.get('port') is None:
                     while True:
                         port_result = session.execute(text("SELECT MAX(port) AS port FROM machine")).fetchone()
-                        port = port_result['port'] + 1 if port_result['port'] is not None else 10000
+                        port = port_result[0] + 1 if port_result[0] is not None else 10000
                         
                         try:
                             machine = Machine(
