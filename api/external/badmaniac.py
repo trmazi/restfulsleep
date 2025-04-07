df7ce423db3751a043f994bbc1db34ca1e5720d4
Trenton Zimmer <trentonzimmer@gmail.com>
2025-04-07 21:18:11 +0000

    Add BM Auth

--- api/external/badmaniac.py
+++ api/external/badmaniac.py
@@ -4,10 +4,12 @@ from api.constants import APIConstants
 
 class BadManiac:
     BM_URL = None
+    BM_KEY = None
 
     @staticmethod
     def update_config(bm_config: Dict[str, Any]) -> None:
         BadManiac.BM_URL = bm_config.get('endpoint', '')
+        BadManiac.BM_KEY = bm_config.get('auth-key', '')
 
     @staticmethod
     def send_link_complete(discord_id: str):
@@ -15,7 +17,7 @@ class BadManiac:
             "discordId": discord_id,
         }
         try:
-            requests.post(f"{BadManiac.BM_URL}/successfulLink", json=request_data)
+            requests.post(f"{BadManiac.BM_URL}/successfulLink", json=request_data, headers={"X-API-Key": BadManiac.BM_KEY})
         except:
             return APIConstants.bad_end('fuck')
 
@@ -28,6 +30,6 @@ class BadManiac:
             }
         }
         try:
-            requests.post(f"{BadManiac.BM_URL}/uploadComplete", json=request_data)
+            requests.post(f"{BadManiac.BM_URL}/uploadComplete", json=request_data, headers={"X-API-Key": BadManiac.BM_KEY})
         except:
             return APIConstants.bad_end('fuck')
